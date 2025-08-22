6e7b850ec63d788ac577117f6f439f442b9a59c6
Juan David Sotelo <131626247+juan-sotelo1994@users.noreply.github.com>
2025-08-21 21:57:02 -0500

    version6

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -10,3 +10,11 @@ from datetime import datetime
 @autenticar.route('/login')
 def login():
     return render_template('login.html')
+
+@autenticar.route('/recupera')
+def recupera():
+    return render_template('/recupera1.html')
+
+@autenticar.route('/verificar')
+def verificar():
+    return render_template('recupera2.html')
